c63e2aece2fab7cf0cc19fc58667c0d825dfe10e
IkrashHaroon <ikrashharoon1236@gmail.com>
2025-11-14 23:37:53 +0500

    Assignment 14

---
+++ allqs.js
@@ -0,0 +1,26 @@
+//  1. Write a program to take “city” name as input from user. If
+// user enters “Karachi”, welcome the user like this:
+// “Welcome to city of lights”
+
+// user = prompt("Enter City Name");
+
+// if (user === "Karachi") {
+//   alert("Welcome to city of lights")
+// }
+// else {
+//     alert(`Welcome to ${user} City`)
+// }
+
+// 2. Write a program to take “gender” as input from user. If the
+// user is male, give the message: Good Morning Sir. If the
+// user is female, give the message: Good Morning Ma’am.
+ gender = prompt("Enter Your Gender");
+ if(gender === "Male") {
+    alert("Good Morning Sir")
+ }
+ else if(gender === "Female") {
+    alert("Good Morning Ma’am")
+ }
+ else{
+    alert("Good Morning, unique human!")
+ }
