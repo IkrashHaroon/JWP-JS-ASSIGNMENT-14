a711ea11fad02e08d8f5cefb0fc33c8bf06aca44
IkrashHaroon <ikrashharoon1236@gmail.com>
2025-11-15 01:02:29 +0500

    Assignment 14

--- allqs.js
+++ allqs.js
@@ -5,26 +5,23 @@
 user = prompt("Enter City Name");
 
 if (user === "Karachi") {
-  alert("Welcome to city of lights")
-}
-else {
-    alert(`Welcome to ${user} City`)
+  alert("Welcome to city of lights");
+} else {
+  alert(`Welcome to ${user} City`);
 }
 
 // 2. Write a program to take “gender” as input from user. If the
 // user is male, give the message: Good Morning Sir. If the
 // user is female, give the message: Good Morning Ma’am.
 
- gender = prompt("Enter Your Gender");
- if(gender === "Male") {
-    alert("Good Morning Sir")
- }
- else if(gender === "Female") {
-    alert("Good Morning Ma’am")
- }
- else{
-    alert("Good Morning, unique human!")
- }
+gender = prompt("Enter Your Gender");
+if (gender === "Male") {
+  alert("Good Morning Sir");
+} else if (gender === "Female") {
+  alert("Good Morning Ma’am");
+} else {
+  alert("Good Morning, unique human!");
+}
 
 // 3. Write a program to take input color of road traffic signal
 // from the user & show the message according to this table:
@@ -33,114 +30,121 @@ else {
 // Yellow Ready to move
 // Green Move now
 
- signalColor = prompt("Enter Traffic Signal Color");
- if(signalColor === "Red"){
-   alert("Must Stop")
- }
- else if(signalColor === "Yellow"){
-    alert("Ready to move")
- }
- else if(signalColor === "Green"){
-    alert("Move now")
- }
-   else{
-      alert("Invalid Color")
-   }
+signalColor = prompt("Enter Traffic Signal Color");
+if (signalColor === "Red") {
+  alert("Must Stop");
+} else if (signalColor === "Yellow") {
+  alert("Ready to move");
+} else if (signalColor === "Green") {
+  alert("Move now");
+} else {
+  alert("Invalid Color");
+}
 // 4. Write a program to take input remaining fuel in car (in
 // litres) from user. If the current fuel is less than 0.25litres,
 // show the message “Please refill the fuel in your car”
- remainingFuel = +prompt("Enter Remaining Fuel in Litres");
+remainingFuel = +prompt("Enter Remaining Fuel in Litres");
 if (remainingFuel < 0.25) {
-alert ("Please refill the fuel in your car")
+  alert("Please refill the fuel in your car");
 } else {
-alert ("You Have Enough Fuel")
+  alert("You Have Enough Fuel");
 }
 
-
- 
+// 5. Run this script, & check whether alert message would be
+// displayed or not. Record the outputs.
 
 var a = 4;
-if (++a === 5){
-alert("given condition for variable a is true");
+if (++a === 5) {
+  alert("given condition for variable a is true");
 }
 var b = 82;
-if (b++ === 83){
-alert("given condition for variable b is true");
+if (b++ === 83) {
+  alert("given condition for variable b is true");
 }
 var c = 12;
-if (c++ === 13){
-alert("condition 1 is true");
+if (c++ === 13) {
+  alert("condition 1 is true");
 }
-if (c === 13){
-alert("condition 2 is true");
+if (c === 13) {
+  alert("condition 2 is true");
 }
-if (++c < 14){
-alert("condition 3 is true");
+if (++c < 14) {
+  alert("condition 3 is true");
 }
-if(c === 14){
-alert("condition 4 is true");
+if (c === 14) {
+  alert("condition 4 is true");
 }
 var materialCost = 20000;
 var laborCost = 2000;
 var totalCost = materialCost + laborCost;
-if (totalCost === laborCost + materialCost){
-alert("The cost equals");
+if (totalCost === laborCost + materialCost) {
+  alert("The cost equals");
 }
-if (true){
-alert("True");
+if (true) {
+  alert("True");
 }
-if (false){
-alert("False");     
+if (false) {
+  alert("False");
 }
-if("car" < "cat"){
-alert("car is smaller than cat");
+if ("car" < "cat") {
+  alert("car is smaller than cat");
 }
+//reults of all conditions
+[
+ "given condition for variable a is true",
+ "condition 2 is true",
+ "condition 4 is true",
+ "The cost equals",
+ "True",
+ "car is smaller than cat"
+];
+
+
+
 // 6. Write a program to take input the marks obtained in three
 // subjects & total marks. Compute & show the resulting
 // percentage on your page. Take percentage & compute
 // grade as per following table:
-// results of all conditions 
-  // three subjects marks
-  var sub1 = +prompt("Enter marks of Subject 1");
-  var sub2 = +prompt("Enter marks of Subject 2");
-  var sub3 = +prompt("Enter marks of Subject 3");
+// results of all conditions
+// three subjects marks
+var sub1 = +prompt("Enter marks of Subject 1");
+var sub2 = +prompt("Enter marks of Subject 2");
+var sub3 = +prompt("Enter marks of Subject 3");
 
-  // total marks
-  var totalMarks = 300;
+// total marks
+var totalMarks = 300;
 
-  // marks obtained
-  var obtained = sub1 + sub2 + sub3;
+// marks obtained
+var obtained = sub1 + sub2 + sub3;
 
-  // percentage
-  var percentage = (obtained / totalMarks) * 100;
+// percentage
+var percentage = (obtained / totalMarks) * 100;
 
-  // grade & remarks
-  var grade = "";
-  var remarks = "";
+// grade & remarks
+var grade = "";
+var remarks = "";
 
-  if (percentage >= 80) {
-    grade = "A-one";
-    remarks = "Excellent";
-  } else if (percentage >= 70) {
-    grade = "A";
-    remarks = "Good";
-  } else if (percentage >= 60) {
-    grade = "B";
-    remarks = "You need to improve";
-  } else {
-    grade = "Fail";
-    remarks = "Sorry";
-  }
+if (percentage >= 80) {
+  grade = "A-one";
+  remarks = "Excellent";
+} else if (percentage >= 70) {
+  grade = "A";
+  remarks = "Good";
+} else if (percentage >= 60) {
+  grade = "B";
+  remarks = "You need to improve";
+} else {
+  grade = "Fail";
+  remarks = "Sorry";
+}
 
-  // show result on page
-  document.write("<h3>Marks Sheet</h3>");
-  document.write("Total marks : " + totalMarks + "<br>");
-  document.write("Marks obtained : " + obtained + "<br>");
-  document.write("Percentage : " + percentage + "%<br>");
-  document.write("Grade : " + grade + "<br>");
-  document.write("Remarks : " + remarks + "<br>");
-
-
+// show result on page
+document.write("<h3>Marks Sheet</h3>");
+document.write("Total marks : " + totalMarks + "<br>");
+document.write("Marks obtained : " + obtained + "<br>");
+document.write("Percentage : " + percentage + "%<br>");
+document.write("Grade : " + grade + "<br>");
+document.write("Remarks : " + remarks + "<br>");
 
 // 7. Guess game:
 // Store a secret number (ranging from 1 to 10) in a variable.
@@ -149,14 +153,14 @@ alert("car is smaller than cat");
 // answer”.
 // b. If the guessed number +1 is the secret number, show
 // “Close enough to the correct answer”.
-secretNumber = 3
-guessNumber =+prompt("Guess the secret number");
-if(secretNumber === guessNumber) {
-alert("“Bingo! Correct answer”")
-}else if(guessNumber + 1 === secretNumber){
-alert("“Close enough to the correct answer”")
+secretNumber = 3;
+guessNumber = +prompt("Guess the secret number");
+if (secretNumber === guessNumber) {
+  alert("“Bingo! Correct answer”");
+} else if (guessNumber + 1 === secretNumber) {
+  alert("“Close enough to the correct answer”");
 } else {
-    alert("Wrong Guess, Try Again!")
+  alert("Wrong Guess, Try Again!");
 }
 // 8. Write a program to check whether the given number is
 // divisible by 3. Show the message to the user if the number
@@ -189,17 +193,13 @@ var T = +prompt("Enter temperature");
 
 if (T > 40) {
   alert("It is too hot outside.");
-} 
-else if (T > 30) {
+} else if (T > 30) {
   alert("The Weather today is Normal.");
-} 
-else if (T > 20) {
+} else if (T > 20) {
   alert("Today’s Weather is cool.");
-} 
-else if (T > 10) {
+} else if (T > 10) {
   alert("OMG! Today’s weather is so Cool.");
-} 
-else {
+} else {
   alert("It is very cold!");
 }
 
@@ -216,32 +216,14 @@ var op = prompt("Enter operation (+, -, *, /, %)");
 
 if (op === "+") {
   alert(num1 + num2);
-} 
-else if (op === "-") {
+} else if (op === "-") {
   alert(num1 - num2);
-} 
-else if (op === "*") {
+} else if (op === "*") {
   alert(num1 * num2);
-} 
-else if (op === "/") {
+} else if (op === "/") {
   alert(num1 / num2);
-} 
-else if (op === "%") {
+} else if (op === "%") {
   alert(num1 % num2);
-} 
-else {
+} else {
   alert("Invalid operation");
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
